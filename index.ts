a9edcc0bc5b376606d84b0f30b4139c089de635c
tran.nicolas <tran.nicolas28@gmail.com>
2021-10-13 12:47:26 -0700

    First try

--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-console.log("hello world")
+import * as fs from "fs";
+import numbers from "./numbers"
+    
+function readFileData(filePath: string): string {
+    let file: string = "";
+    file = fs.readFileSync(filePath).toString();
+    return file;
+}
+
+function displayInput(input : string) : void {
+    for(let i = 0; i < input.length ; i++)
+    {
+        console.log(`${i} : ${input[i]}`);
+    }
+}
+
+function parseNumber(stringTable : string): number {
+    const data : string = readFileData("ocr-input.txt");
+    let parsed = [0,564];
+    let x = 0;
+    stringTable
+        .split('')
+        .forEach(e=> x += e.charCodeAt(0));
+    return x;
+}
+
+console.log(parseNumber(" |  |  | "));
+//console.log(readFileData("ocr-input.txt"));
+//displayInput(readFileData("ocr-input.txt"));
+//parseNumber();
+//console.log(numbers.get(2))
