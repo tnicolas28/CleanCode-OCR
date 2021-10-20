180cad8c3a6141cbcb5e9eb5a4ce3e6e9c41b6a8
tran.nicolas <tran.nicolas28@gmail.com>
2021-10-20 15:31:16 -0700

    User story 1 done, need to clean

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as fs from "fs";
-import numbers from "./numbers"
+
     
 function readFileData(filePath: string): string {
     let file: string = "";
@@ -8,24 +8,34 @@ function readFileData(filePath: string): string {
 }
 
 function displayInput(input : string) : void {
-    for(let i = 0; i < input.length ; i++)
+    console.log(input);
+    for (let i = 0; i < input.length ; i++)
     {
         console.log(`${i} : ${input[i]}`);
     }
 }
 
-function parseNumber(stringTable : string): number {
-    const data : string = readFileData("ocr-input.txt");
-    let parsed = [0,564];
+function parseNumber(number : string): number {
+    let parsed = [4462,2820,3581,3765,3503,3581,4225,2946,4777,4133];
     let x = 0;
-    stringTable
-        .split('')
-        .forEach(e=> x += e.charCodeAt(0));
-    return x;
+    number.split('')
+          .forEach((e, i)=> x += e.charCodeAt(0)*(i+1));
+    return parsed.indexOf(x);
 }
 
-console.log(parseNumber(" |  |  | "));
-//console.log(readFileData("ocr-input.txt"));
-//displayInput(readFileData("ocr-input.txt"));
-//parseNumber();
-//console.log(numbers.get(2))
+
+function parseLine(line: string): string {
+    let number = "";
+    let lines = line.split('\n');
+    if (lines[0].length > 27) {
+        throw new Error();
+    }
+    for (let i = 0; i < lines[0].length / 3; i++) {
+        number += parseNumber(lines[0].substring(3*i,3*(i+1)) + lines[1].substring(3*i,3*(i+1)) + lines[2].substring(3*i,3*(i+1)));
+    }
+    return number;
+}
+
+function parseFile(file: string): string[] {
+    return file.split("  \n").map(line=>parseLine(line));
+}
